65d8e138a5442a3b4ac243857202a71d55ef4233
Mark <tractor2554393@yandex.by>
2019-05-03 11:13:23 +0300

    Deposit bank and cryto - done. No responsive for 13.3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let currentSendCrypto = document.querySelector('.current-crypto');
 let sendCryptoList = document.querySelector('.send-cryptocurency-list');
-let depositNavigation = document.querySelector('.choice-navigation');
+let personalAccountNavigation = document.querySelector('.personal-account-navigation-lists');
 let withdrawNavigation = document.querySelector(".choice-withdraw-navigation")
 
 $('#modalGetLogin').change(function(){
@@ -17,72 +17,30 @@ $('#modalGetSign').change(function(){
     }
 });
 
-$('#deposit').change(function() {
-   if($("#deposit:checked")) {
-       $('#depositBlock').show();
-       $('#withdrawBlock').hide();
-   }
-});
-
-
-// переключение на вывод денег
-$('#withdraw').change(function() {
-    if($("#withdraw:checked")) {
-        $('#withdrawBlock').show();
-        $('#depositBlock').hide();
-    }
-});
-
-
-//выдвигается список с криптой
-$(currentSendCrypto).click(function () {
-    if ($(sendCryptoList).is(":hidden")) {
-        $(sendCryptoList).show();
-    } else if ($(sendCryptoList).is(":visible")) {
-        $(sendCryptoList).hide();
-    }
-});
-
-
-//выбор крипты для пополнения
-$(sendCryptoList).click(function (e) {
-    currentSendCrypto.textContent = e.target.textContent;
-    $(sendCryptoList).hide();
-});
-
 
 //Способ пополнения
-$(depositNavigation).change(function () {
-   if ($('#depositBankTransaction').is(':checked')) {
-
-       $('.bank-transfer-variant').toggleClass('choice-navigation-variant-check',true);
-       $('.crypto-variant').toggleClass('choice-navigation-variant-check',false);
-
-
-       $('#bankTransaction').show();
-       $('#cryptocurrency').hide();
-    } else if ($('#depositCrypto').is(':checked')) {
-       $('.crypto-variant').toggleClass('choice-navigation-variant-check',true);
-       $('.bank-transfer-variant').toggleClass('choice-navigation-variant-check',false);
-
-       $('#bankTransaction').hide();
-       $('#cryptocurrency').show();
+$(personalAccountNavigation).change(function () {
+   if ($('#btnDepositBank').is(':checked')) {
+       $('#bankDepositBlock').show();
+       $('#cryptoDepositBlock').hide();
+    } else if ($('#btnDepositCrypto').is(':checked')) {
+       $('#cryptoDepositBlock').show();
+       $('#bankDepositBlock').hide();
    }
 });
-//Способ вывода
 
-$(withdrawNavigation).change(function () {
-   if ($('#withdrawBankTransaction').is(':checked')) {
-        $('.withdraw-bank-variant').toggleClass('choice-navigation-variant-check',true);
-        $('.withdraw-crypto-variant').toggleClass('choice-navigation-variant-check',false);
 
-        $('#withdrawCryptoBlock').hide();
-        $('#withdrawBankBlock').show();
-    } else if ($('#withdrawCrypto').is(':checked')) {
-       $('.withdraw-bank-variant').toggleClass('choice-navigation-variant-check',false);
-       $('.withdraw-crypto-variant').toggleClass('choice-navigation-variant-check',true);
+// Mobile personal account
+//Показать банковский деп (моб)
 
-       $('#withdrawCryptoBlock').show();
-       $('#withdrawBankBlock').hide();
-   }
+$('#mobileBankTransferBtn').click(function () {
+    $('#mobileDepositBlock').show();
+    $('#mobileCryptoBlock').hide();
+});
+
+//Показать крипто деп (моб)
+
+$('#mobileBtnDepositCrypto').click(function () {
+    $('#mobileCryptoBlock').show();
+    $('#mobileDepositBlock').hide();
 });
